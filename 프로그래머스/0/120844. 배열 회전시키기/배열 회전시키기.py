e12e783a62b21e9954df3db2b478630f4c1ad0a5
J1sooo <149297374+J1sooo@users.noreply.github.com>
2024-11-02 11:05:16 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,6 +1,6 @@
 def solution(numbers, direction):
     if direction=="right":
-        return [numbers[len(numbers)-1]]+numbers[:len(numbers)-1]
+        return [numbers[-1]]+numbers[:len(numbers)-1]
     else:
-        return numbers[1:len(numbers)]+[numbers[0]]
+        return numbers[1:]+[numbers[0]]
     
