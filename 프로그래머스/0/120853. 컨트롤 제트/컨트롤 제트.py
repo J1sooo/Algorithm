9d09ef2b68086096b6c0bf7839a541babf1b5b53
J1sooo <149297374+J1sooo@users.noreply.github.com>
2024-11-10 14:05:23 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -2,8 +2,8 @@ def solution(s):
     l1=[]
     answer = 0
     for i in s.split():
-        if i.lstrip('-').isdigit():
-            l1.append(int(i))
-        elif i=='Z':
+        if i=='Z':
             l1.pop()
+        else:
+            l1.append(int(i))
     return sum(l1)
